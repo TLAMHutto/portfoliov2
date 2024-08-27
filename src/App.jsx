e6b1ce2a746f4849e9f0ebc0c294d098c9f14b26
TLAMHutto <keatonhutto8@gmail.com>
2024-08-26 19:40:27 -0500

    breakpoints working!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
 import Carousel from './carousel/MobileCarousel'
 import './index.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./carousel/mobileCarousel.css"
-function App() {
+import ThreeBoxes from './desktop/ThreeBoxes';
+import { useState, useEffect } from 'react';
 
-  return (
-    <Carousel />
-  )
-}
+const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-export default App
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile ? <Carousel /> : <ThreeBoxes />;
+};
+
+export default App;
---
+++ src/desktop/ThreeBoxes.jsx
@@ -0,0 +1,13 @@
+import "./threeboxes.css";
+
+function ThreeBoxes() {
+  return (
+    <div className="container">
+      <div className="column column1"></div>
+      <div className="column column2"></div>
+      <div className="column column3"></div>
+    </div>
+  );
+}
+
+export default ThreeBoxes;
