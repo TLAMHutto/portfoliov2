87700d36eb21cde6e3c1e68be34d218c55b73a92
TLAMHutto <keatonhutto8@gmail.com>
2024-08-27 18:53:21 -0500

    logo

--- src/desktop/ThreeBoxes.jsx
+++ src/desktop/ThreeBoxes.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
-import './ThreeBoxes.css'; // Assuming you have some CSS for styling
+import './ThreeBoxes.css';
+import white from '../imgs/white.png'
 
 function ThreeBoxes() {
 
   return (
     <div className="container">
       <div className="column column1"></div>
-      <div className="column column2"></div>
+      <div className="column column2">
+        <img src={ white } className='logo'></img>
+      </div>
       <div className="column column3"></div>
     </div>
   );
