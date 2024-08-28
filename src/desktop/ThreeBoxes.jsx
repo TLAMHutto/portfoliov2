9639b68b23fb594f2ac52949b146223a5bdea616
TLAMHutto <keatonhutto8@gmail.com>
2024-08-27 19:35:19 -0500

    links-logos

--- src/desktop/ThreeBoxes.jsx
+++ src/desktop/ThreeBoxes.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
 import './ThreeBoxes.css';
-import white from '../imgs/white.png'
-
+import gold from '../imgs/gold.png'
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 function ThreeBoxes() {
 
   return (
     <div className="container">
       <div className="column column1"></div>
       <div className="column column2">
-        <img src={ white } className='logo'></img>
+        <img src={ gold } className='logo'></img>
+        <div className="icon-row">
+          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="mailto:your.email@example.com">
+            <FaEnvelope />
+          </a>
+        </div>
       </div>
       <div className="column column3"></div>
     </div>
