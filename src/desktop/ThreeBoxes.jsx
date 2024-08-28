f2f1770826747674f40316ae67f5c6331f733833
TLAMHutto <keatonhutto8@gmail.com>
2024-08-27 20:32:11 -0500

    about-me

--- src/desktop/ThreeBoxes.jsx
+++ src/desktop/ThreeBoxes.jsx
@@ -9,6 +9,20 @@ function ThreeBoxes() {
       <div className="column column1"></div>
       <div className="column column2">
         <img src={ gold } className='logo'></img>
+        <div className='aboutMe-container'>
+          {/* <h3 className='name'>
+            Im Keaton. . .
+          </h3> */}
+          <p className='aboutMe'>
+          I am a full stack engineer with 6+ years experience,
+        proficient in designing and delivering scalable solutions with expertise in
+        technologies including JavaScript frameworks (React, Angular, Electron.js,
+        Three.js) and Python. Skilled in integrating cloud technologies and popular
+        backend solutions (e.g., Node.js, Express, Django, AWS, Docker) to build
+        robust and efficient SaaS applications.
+          </p>
+
+        </div>
         <div className="icon-row">
           <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
             <FaGithub />
